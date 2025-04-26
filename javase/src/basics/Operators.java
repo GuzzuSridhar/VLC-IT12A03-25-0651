129f33200afff3d996113aeda1d97af6c994ef85
GuzzuSridhar <132375692+GuzzuSridhar@users.noreply.github.com>
2025-04-26 14:45:32 +0530

    operators

---
+++ javase/src/basics/Operators.java
@@ -0,0 +1,21 @@
+package basics;
+
+public class Operators {
+    public static void main(String[] args) {
+        int x = 10, y = 20;
+        // Arithmetic Operators
+        System.out.println(x + y); // addition operator "+"
+        System.out.println(x - y); // subtraction operator "-"
+        System.out.println(x * y); // Multiplication operator "*"
+        System.out.println(x / y); // Division operator "/"
+        System.out.println(x % y); // Modulus operator "/"
+
+        System.out.println(x++); // post increment "++"
+        System.out.println(++x); // pre increment "++"
+        System.out.println(x--); // post decrement "--"
+        System.out.println(--x); // pre decrement "--"
+        // Assignment Operators
+        // Comparision Operators
+        // Logical Operators
+    }
+}
