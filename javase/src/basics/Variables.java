fdc22878100564d6f9079c7dd46c5a5bbd55e8f3
GuzzuSridhar <132375692+GuzzuSridhar@users.noreply.github.com>
2025-04-26 13:12:01 +0530

    add variables

---
+++ javase/src/basics/Variables.java
@@ -0,0 +1,18 @@
+package basics;
+
+public class Variables {
+    public static void main(String[] args) {
+        // declare single variable
+        int x = 10; // variable with assignment of a value
+        int y; // variable with no assignment
+
+        // declare more than 1 variable of same type
+        int a = 20, b, c = 89;
+        // assigning a value to variable b
+        b = 9;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+
+    }
+}
