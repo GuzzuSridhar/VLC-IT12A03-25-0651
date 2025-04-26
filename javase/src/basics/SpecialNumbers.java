0032a6be1c093708983eb1addb087460fad508b2
GuzzuSridhar <132375692+GuzzuSridhar@users.noreply.github.com>
2025-04-26 14:15:31 +0530

    add sp numbers

---
+++ javase/src/basics/SpecialNumbers.java
@@ -0,0 +1,20 @@
+package basics;
+
+public class SpecialNumbers {
+    public static void main(String[] args) {
+        // special numbers
+
+        // octal number assignmnet. any number prefixed with zero
+        int octNum = 071;
+        System.out.println(octNum);
+
+        // Binary number assignment. any number with prefix of zerob "0b"
+        int binNum = 0b011;
+        System.out.println(binNum);
+
+        // Hex number assignment. any number with prefix of zerob "0b"
+        int hexNum = 0x11;
+        System.out.println(hexNum);
+
+    }
+}
