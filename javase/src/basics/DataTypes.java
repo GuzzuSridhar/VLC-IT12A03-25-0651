74721b80bd26f6da7124ab569fc076c7ded17cbb
GuzzuSridhar <132375692+GuzzuSridhar@users.noreply.github.com>
2025-04-26 12:46:53 +0530

    add basics

---
+++ javase/src/basics/DataTypes.java
@@ -0,0 +1,49 @@
+package basics;
+
+public class DataTypes {
+    public static void main(String[] args) {
+        // Single Line Comment
+        /*
+         * Multi Line comment
+         * line 1
+         * lin 2
+         * line 3
+         */
+        System.out.println("Data Types");
+
+        /*
+         * primitive types
+         * 1. byte
+         * 2. short
+         * 3. int
+         * 4. long
+         * 5. float
+         * 6. double
+         * 7. boolean
+         * 8. char
+         */
+        // declare a byte type variable
+        // 8 bit signed integer
+        byte age = 26;
+
+        // declare a short type variable
+        // 16 bit signed integer
+        short count = 78;
+
+        // declare a int type variable
+        // 32 bit signed integer
+        int counts = 190;
+
+        // declare a long type variable
+        // 64 bit signed integer
+        long months = 189;
+
+        // printing the variable values
+        System.out.println(age);
+        System.out.println(count);
+        System.out.println(counts);
+        System.out.println(months);
+
+    }
+
+}
