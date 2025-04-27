f344fefa8a0d29a523b5ca9ca7b7b0d67acc2d5f
GuzzuSridhar <132375692+GuzzuSridhar@users.noreply.github.com>
2025-04-27 13:04:45 +0530

    add loops

---
+++ javase/src/basics/Loops.java
@@ -0,0 +1,19 @@
+package basics;
+
+public class Loops {
+    public static void main(String[] args) {
+        // while loop
+        // dowhile loop
+        // for loop
+        for (int i = 1; i <= 10; i++) {
+            System.out.print(i + " ");
+        }
+        // print reverse numbers
+        for (int i = 10; i > 0; i--) {
+            System.out.print(i + " ");
+        }
+
+        String test = "Hello";
+
+    }
+}
