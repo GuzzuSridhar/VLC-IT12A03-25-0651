1311cbfaaf246a00f7a9ea7cb218fdfbb918c36a
GuzzuSridhar <132375692+GuzzuSridhar@users.noreply.github.com>
2025-04-27 10:12:20 +0530

    add input

---
+++ javase/src/basics/UserInput.java
@@ -0,0 +1,18 @@
+package basics;
+
+import java.util.Scanner;
+
+public class UserInput {
+    public static void main(String[] args) {
+        // have a Scanner object to take use Input
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter your name: ");
+        String name = scan.nextLine();
+        System.out.print("Enter your Age: ");
+        int age = scan.nextInt();
+
+        System.out.printf("Welcome %s, you are %d years old \n", name, age);
+        System.out.println("Welcome " + name + ", you are " + age + " years old" + "\n");
+
+    }
+}
