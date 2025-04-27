3b9fd4c02af59953755d4f0f4ef67074238db90b
GuzzuSridhar <132375692+GuzzuSridhar@users.noreply.github.com>
2025-04-27 13:26:53 +0530

    add nested loops

--- javase/src/basics/Loops.java
+++ javase/src/basics/Loops.java
@@ -18,5 +18,31 @@ public class Loops {
             System.out.print(test.charAt(i));
         }
 
+        System.out.println("\n-----------------------Whiile loop--------------------");
+
+        // while Loop
+        int loopVar = 1;
+        while (loopVar < 11) {
+            System.out.println(loopVar);
+            loopVar++;
+        }
+
+        System.out.println("\n-----------------------DoWhiile loop--------------------");
+        // doWhile (one iteration mandated)
+        loopVar = 100;
+        do {
+            System.out.println(loopVar);
+            loopVar++;
+        } while (loopVar < 11);
+
+        System.out.println("\n-----------------------Nested loop--------------------");
+        // nested Loops
+        for (int i = 1; i <= 5; i++) {
+            System.out.println(" Multiplication Table of " + i);
+            for (int j = 1; j <= 10; j++) {
+                System.out.println(i + " x " + j + " = " + (i * j));
+            }
+        }
+
     }
 }
