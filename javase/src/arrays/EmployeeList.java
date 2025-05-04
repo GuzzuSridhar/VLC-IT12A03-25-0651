a55af63af7db100a976684467f843bf43c622806
GuzzuSridhar <132375692+GuzzuSridhar@users.noreply.github.com>
2025-05-04 09:42:28 +0530

    add arr

--- javase/src/arrays/EmployeeList.java
+++ javase/src/arrays/EmployeeList.java
@@ -6,26 +6,29 @@ import classesandobjects.Employee;
 
 public class EmployeeList {
     public static void main(String[] args) {
-        Employee[] empList = new Employee[5];
+        Employee[] empList = new Employee[2];
         Scanner scan = new Scanner(System.in);
 
         // using a loop to populate the array
-        for (Employee employee : empList) {
-            employee = new Employee();
+        for (int i = 0; i < empList.length; i++) {
+            empList[i] = new Employee();
             System.out.print("Enter Employee Name: ");
-            employee.name = scan.nextLine();
+            empList[i].name = scan.nextLine();
             System.out.print("Enter Employee age: ");
-            employee.age = scan.nextInt();
+            empList[i].age = scan.nextInt();
+            scan.nextLine();
             System.out.print("Enter Employee dept: ");
-            employee.dept = scan.nextLine();
+            empList[i].dept = scan.nextLine();
             System.out.print("Enter Employee sal: ");
-            employee.sal = scan.nextFloat();
+            empList[i].sal = scan.nextFloat();
+            scan.nextLine();
         }
         scan.close();
 
         // Printing the array
         for (Employee employee : empList) {
-            System.out.println(employee.name + " " + employee.age + " " + employee.dept + " " + employee.sal);
+            System.out.println(employee.name + " " + employee.age + " " + employee.dept +
+                    " " + employee.sal);
         }
 
     }
