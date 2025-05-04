de977157ff1a623353fab2f4671e9bcb07d6df2d
GuzzuSridhar <132375692+GuzzuSridhar@users.noreply.github.com>
2025-05-04 08:09:17 +0530

    add class

---
+++ javase/src/classesandobjects/Book.java
@@ -0,0 +1,13 @@
+package classesandobjects;
+
+// bluprint of a book
+// main method is not mandatory
+public class Book {
+    // attributes
+    // instance variables
+    String title;
+    String author;
+    String genre;
+    int pubYear;
+    int pages;
+}
---
+++ javase/src/classesandobjects/Library.java
@@ -0,0 +1,31 @@
+package classesandobjects;
+
+// storing the list of books
+public class Library {
+    public static void main(String[] args) {
+        // create instances of books
+        Book b001 = new Book();
+        Book b002 = new Book();
+
+        // populate the values (dot) notation is used to access class attributes
+
+        // book1
+        b001.title = "The Book Thief";
+        b001.author = "Markus Zusak";
+        b001.genre = "Suspense";
+        b001.pages = 1000;
+        b001.pubYear = 2001;
+        // book2
+        b002.title = "All the Light We Cannot See ";
+        b002.author = "Anthony Doerr";
+        b002.genre = "Horror";
+        b002.pages = 1040;
+        b002.pubYear = 2021;
+
+        // access the instances and print the details
+        System.out.println(
+                b001.title + " - " + b001.author + " - " + b001.genre + " - " + b001.pages + " - " + b001.pubYear);
+        System.out.println(
+                b002.title + " - " + b002.author + " - " + b002.genre + " - " + b002.pages + " - " + b002.pubYear);
+    }
+}
