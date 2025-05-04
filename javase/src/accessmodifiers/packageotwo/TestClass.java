60666be297431b0b23e9738024d58fd651baa4dc
GuzzuSridhar <132375692+GuzzuSridhar@users.noreply.github.com>
2025-05-04 10:19:15 +0530

    add test

---
+++ javase/src/accessmodifiers/packageotwo/TestClass.java
@@ -0,0 +1,8 @@
+package accessmodifiers.packageotwo;
+
+// at a class level we can use public default access modifiers 
+public class TestClass {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
