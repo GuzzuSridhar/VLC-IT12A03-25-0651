32baf90c7ca636e5932bdb2ea43b7f4dec1012f1
GuzzuSridhar <132375692+GuzzuSridhar@users.noreply.github.com>
2025-05-17 07:34:06 +0530

    add loop

--- javase/src/exceptions/InputError.java
+++ javase/src/exceptions/InputError.java
@@ -10,11 +10,19 @@ public class InputError {
             System.out.print("Enter a Number: ");
             int i = input.nextInt();
             System.out.println(i);
-        } catch (InputMismatchException e) {
+        } catch (InputMismatchException e) { // executed only when a exception occurs (raised)
             System.out.println("Enter a valid Number");
-        } finally {
+        } finally { // executed always
             input.close();
         }
         System.out.println("Other logic......");
+
+        // Scanner input = new Scanner(System.in);
+
+        // System.out.print("Enter a Number: ");
+        // int i = input.nextInt();
+        // System.out.println(i);
+        // input.close();
+        // System.out.println("Other logic......");
     }
 }
