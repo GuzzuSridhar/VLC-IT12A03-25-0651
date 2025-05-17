6f31a37f20f8491176f4981b0f353fe97dfb7583
GuzzuSridhar <132375692+GuzzuSridhar@users.noreply.github.com>
2025-05-17 10:07:41 +0530

    add deser

--- javase/src/serialization/Employee.java
+++ javase/src/serialization/Employee.java
@@ -1,8 +1,8 @@
 package serialization;
 
-import java.io.FileOutputStream;
+import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.ObjectOutputStream;
+import java.io.ObjectInputStream;
 import java.io.Serializable;
 
 public class Employee implements Serializable {
@@ -35,18 +35,30 @@ public class Employee implements Serializable {
         return "Employee [id=" + id + ", name=" + name + "]";
     }
 
-    public static void main(String[] args) {
-        Employee employee = new Employee(100, "Alex");
+    public static void main(String[] args) throws ClassNotFoundException {
+        // Employee employee = new Employee(100, "Alex");
         // write the employee object to a file
         // Serialization
-        try {
-            FileOutputStream out = new FileOutputStream("emp.ser");
-            ObjectOutputStream objectOutputStream = new ObjectOutputStream(out);
-            objectOutputStream.writeObject(employee);
-            System.out.println("Object is written to a file");
-            objectOutputStream.close();
-            out.close();
+        // try {
+        // FileOutputStream out = new FileOutputStream("emp.ser");
+        // ObjectOutputStream objectOutputStream = new ObjectOutputStream(out);
+        // objectOutputStream.writeObject(employee);
+        // System.out.println("Object is written to a file");
+        // objectOutputStream.close();
+        // out.close();
 
+        // } catch (IOException e) {
+        // e.printStackTrace();
+        // }
+
+        // deserialization - read from the .ser file
+        try {
+            FileInputStream fileInputStream = new FileInputStream("emp.ser");
+            ObjectInputStream in = new ObjectInputStream(fileInputStream);
+            Employee readEmp = (Employee) in.readObject();
+            System.out.println(readEmp);
+            in.close();
+            fileInputStream.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
