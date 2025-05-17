a578ff4aeea7145cd6b58c337b036f6c7d411322
GuzzuSridhar <132375692+GuzzuSridhar@users.noreply.github.com>
2025-05-17 07:49:58 +0530

    add zero

--- javase/src/exceptions/ZeroDivide.java
+++ javase/src/exceptions/ZeroDivide.java
@@ -1,11 +1,17 @@
 package exceptions;
 
+import java.util.InputMismatchException;
+
 public class ZeroDivide {
     public static void main(String[] args) {
         int x = 10, y = 0;
         try {
             System.out.println(x / y); // graceful exit
         } catch (ArithmeticException e) {
+            System.out.println(e.getMessage());
+        } catch (InputMismatchException e) {
+            System.out.println(e.getMessage());
+        } catch (Exception e) { // generic exception (super class for all types of exceptions)
             System.out.println(e.getMessage());
         }
     }
