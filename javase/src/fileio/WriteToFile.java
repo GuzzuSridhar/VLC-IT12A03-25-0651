49e51014de52013c86d04fa9b30c3acd669e0c49
GuzzuSridhar <132375692+GuzzuSridhar@users.noreply.github.com>
2025-05-17 09:15:19 +0530

    add bufferwrite

--- javase/src/fileio/WriteToFile.java
+++ javase/src/fileio/WriteToFile.java
@@ -1,5 +1,6 @@
 package fileio;
 
+import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
 
@@ -7,6 +8,7 @@ public class WriteToFile {
     public static void main(String[] args) {
         // create a file writer object
         try {
+            // using the FileWriter, one character at a time is written
             // FileWriter writer = new FileWriter("myfile.txt"); // write to the file in
             // current dir
             FileWriter writer = new FileWriter("myfile.txt", true); // update to the file in current dir
@@ -15,6 +17,20 @@ public class WriteToFile {
             writer.close();
         } catch (IOException e) {
             e.printStackTrace();
+        }
+
+        // use the bufferred writer to buffer the text and write to a file
+        // the buffer size is 8kb
+
+        try {
+            BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter("myfile.txt", true));
+            bufferedWriter.write("Using buffered writer.\n");
+            bufferedWriter.write("Hello, this is a test.\n");
+            bufferedWriter.write("Hello, this is a test.\n");
+            bufferedWriter.write("Hello, this is a test.\n");
+            bufferedWriter.close();
+        } catch (IOException ie) {
+            ie.getMessage();
         }
     }
 }
