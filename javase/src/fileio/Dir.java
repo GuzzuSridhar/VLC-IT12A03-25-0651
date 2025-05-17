9332787543f4b98db80e5737602cdd675c9b81e9
GuzzuSridhar <132375692+GuzzuSridhar@users.noreply.github.com>
2025-05-17 09:49:52 +0530

    add dir listing

---
+++ javase/src/fileio/Dir.java
@@ -0,0 +1,22 @@
+package fileio;
+
+import java.io.File;
+
+public class Dir {
+    public static void main(String[] args) {
+        File file = new File("dir1"); // any file name without an extension is a directory
+        file.mkdir();
+
+        File file1 = new File("dir2\\dir3"); // create a directory with sub directory
+        file1.mkdirs();
+
+        System.out.println("--------------");
+        // director listing
+        File myProjects = new File("D:\\VLC-IT12A03-25-0651\\javase\\src");
+        String[] files = myProjects.list();
+        for (String fname : files) {
+            System.out.println(fname);
+        }
+
+    }
+}
