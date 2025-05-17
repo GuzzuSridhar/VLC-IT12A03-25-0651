e972d4b40f6c56f11401e6881f538b4046e300ee
GuzzuSridhar <132375692+GuzzuSridhar@users.noreply.github.com>
2025-05-17 09:30:14 +0530

    add buf read

--- javase/src/fileio/ReadFile.java
+++ javase/src/fileio/ReadFile.java
@@ -1,11 +1,12 @@
 package fileio;
 
+import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 
 public class ReadFile {
     public static void main(String[] args) throws IOException {
-        // File Reader object
+        // File Reader object (one character at a time)
         FileReader fileReader = new FileReader("myfile.txt");
         int ch;
         while ((ch = fileReader.read()) != -1) {
@@ -14,5 +15,15 @@ public class ReadFile {
             // System.out.print(ch);
         }
         fileReader.close();
+
+        System.out.println("----------------------");
+
+        // buffered reader (8 kb at a time)
+        FileReader reader = new FileReader("myfile.txt");
+        BufferedReader bufferedReader = new BufferedReader(reader);
+        while (bufferedReader.ready()) {
+            System.out.println(bufferedReader.readLine());
+        }
+        bufferedReader.close();
     }
 }
